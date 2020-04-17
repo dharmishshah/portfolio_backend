e68bc0ca459367d7a3f9f381d6cf1e544abd07cd
dharmishshah <shah.dharm@husky.neu.edu>
2020-04-17 03:14:39 -0400

    changes

--- Portfolio_backend/settings.py
+++ Portfolio_backend/settings.py
@@ -21,7 +21,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
