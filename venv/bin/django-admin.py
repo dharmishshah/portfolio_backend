44687707a62bf37626e98ffe9f2b45d54aac1091
dharmishshah <shah.dharm@husky.neu.edu>
2020-04-17 02:25:20 -0400

    first backend commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dharmish/work/personal_projects/Portfolio/Portfolio_backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
